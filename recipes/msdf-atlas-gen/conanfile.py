1c19e2572b767b3d67cba12ddd577262f4081802
Dandielo <dandielo@iceshard.net>
2022-05-23 20:20:04 +0200

    Initial work required for msdf-atlas-gen to be packaged.

--- recipes/msdf-atlas-gen/conanfile.py
+++ recipes/msdf-atlas-gen/conanfile.py
@@ -1,11 +1,11 @@
 from conans import ConanFile, Meson, tools
 import os
 
-class MsdfGenConanRecipe(ConanFile):
-    name = "msdfgen"
+class MsdfAtlasGenConanRecipe(ConanFile):
+    name = "msdf-atlas-gen"
     license = "MIT"
-    description = "Multi-channel signed distance field generator."
-    url = "https://github.com/Chlumsky/msdfgen"
+    description = "MSDF font atlas generator."
+    url = "https://github.com/Chlumsky/msdf-atlas-gen"
 
     # Settings and options
     settings = "os", "compiler", "arch", "build_type"
@@ -17,16 +17,16 @@ class MsdfGenConanRecipe(ConanFile):
         "fPIC": True
     }
 
-    source_dir = "{name}-{version}"
+    # source_dir = "{name}-{version}"
 
     # Additional files to export
-    exports_sources = ["patches/*", "Findmsdfgen.cmake"]
+    exports_sources = ["patches/*"]
 
     # Iceshard conan tools
-    python_requires = "conan-iceshard-tools/0.8.0@iceshard/stable"
+    python_requires = "conan-iceshard-tools/0.8.2@iceshard/stable"
     python_requires_extend = "conan-iceshard-tools.IceTools"
 
-    requires = "freetype/2.12.1@iceshard/stable"
+    requires = "msdfgen/1.9.2@iceshard/stable"
 
     # Initialize the package
     def init(self):
@@ -39,26 +39,33 @@ class MsdfGenConanRecipe(ConanFile):
             del self.options.fPIC
 
     def ice_build(self):
+        self.run("cd artery-font-format && git submodule update --init")
+
+        self.ice_generate()
         self.ice_apply_patches()
-        self.ice_run_cmake()
+
+        definitions = { }
+        definitions['MSDF_ATLAS_GEN_BUILD_STANDALONE'] = False
+        definitions['MSDF_ATLAS_GEN_MSDFGEN_EXTERNAL'] = True
+        self.ice_run_cmake(definitions)
 
     def ice_package(self):
-        self.copy("Findmsdfgen.cmake", src=".", dst=".")
         self.copy("LICENSE.txt", src=self._ice.source_dir, dst="LICENSE")
 
         self.copy("*.h*", "include/msdfgen/core", src="{}/core".format(self._ice.source_dir), keep_path=True)
         self.copy("*.h*", "include/msdfgen/ext", src="{}/ext".format(self._ice.source_dir), keep_path=True)
         self.copy("*.h*", "include/msdfgen/skia", src="{}/skia".format(self._ice.source_dir), keep_path=True)
-        self.copy("*.h*", "include/msdfgen/include", src="{}/include".format(self._ice.source_dir), keep_path=True)
         self.copy("msdfgen.h", "include/msdfgen", src="{}".format(self._ice.source_dir), keep_path=True)
         self.copy("msdfgen-*.h", "include/msdfgen", src="{}".format(self._ice.source_dir), keep_path=True)
 
         build_dir = self._ice.build_dir
         if self.settings.os == "Windows":
-            self.copy("*.lib", dst="lib", src="{}/../build/lib".format(build_dir), keep_path=False)
-            self.copy("*.pdb", dst="lib", src="{}/../build/lib".format(build_dir), keep_path=False)
+            build_dir = os.path.join(build_dir, "../build")
+
+            self.copy("*.lib", dst="lib", src="{}/lib".format(build_dir), keep_path=False)
+            self.copy("*.pdb", dst="lib", src="{}/lib".format(build_dir), keep_path=False)
         if self.settings.os == "Linux":
-            self.copy("*.a", dst="lib", src="{}/../build/lib".format(build_dir), keep_path=False)
+            self.copy("*.a", dst="lib", src="{}/lib".format(build_dir), keep_path=False)
 
     def package_info(self):
         self.cpp_info.libdirs = ['lib']
