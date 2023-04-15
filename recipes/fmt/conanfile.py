e3eb15d3f9f0a32215f4f3606950cb779da90e26
Dandielo <dandielo@iceshard.net>
2023-04-15 14:18:22 +0900

    Updated fmt for Conan v2

--- recipes/fmt/conanfile.py
+++ recipes/fmt/conanfile.py
@@ -1,6 +1,4 @@
-from conans import ConanFile, MSBuild, tools
-from shutil import copyfile
-import os
+from conan import ConanFile
 
 class FmtConan(ConanFile):
     name = "fmt"
@@ -15,38 +13,32 @@ class FmtConan(ConanFile):
     default_options = { "fPIC":True }
 
     # Iceshard conan tools
-    python_requires = "conan-iceshard-tools/0.8.2@iceshard/stable"
+    python_requires = "conan-iceshard-tools/0.9.0@iceshard/stable"
     python_requires_extend = "conan-iceshard-tools.IceTools"
 
-    # Initialize the package
-    def init(self):
-        self.ice_init("cmake")
-        self.build_requires = self._ice.build_requires
+    ice_generator = "cmake"
+    ice_toolchain = "cmake"
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    # Build both the debug and release builds
     def ice_build(self):
-        definitions = { }
-        definitions['FMT_INSTALL'] = False
-        definitions['FMT_TEST'] = False
-        definitions['FMT_DOC'] = False
+        variables = { }
+        variables['FMT_INSTALL'] = False
+        variables['FMT_TEST'] = False
+        variables['FMT_DOC'] = False
         if self.settings.os != "Windows":
-            definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
-        self.ice_run_cmake(definitions)
+            variables['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
+        self.ice_run_cmake(variables=variables)
 
-    def ice_package(self):
-        self.copy("LICENSE.rst", src=self._ice.source_dir, dst="LICENSES")
+    def ice_package_sources(self):
+        self.ice_copy("LICENSE.rst", src=".", dst="LICENSES")
+        self.ice_copy("*.h", src="include", dst="include", keep_path=True)
 
-        self.copy("*.h", "include", src="{}/include".format(self._ice.source_dir), keep_path=True)
-
-        build_dir = self._ice.build_dir
-        if self.settings.os == "Windows":
-            self.copy("*.lib", "lib", build_dir, keep_path=False)
-        if self.settings.os == "Linux":
-            self.copy("*.a", "lib", build_dir, keep_path=False)
+    def ice_package_artifacts(self):
+        self.ice_copy("*.lib", src=".", dst="lib", keep_path=False)
+        self.ice_copy("*.a", src=".", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libdirs = ["lib"]
