beef8cbc3bd6e6d0abe396befc9930db3f74e92e
Dandielo <dandielo@iceshard.net>
2023-04-15 14:14:05 +0900

    Updated msdfgen for Conan v2

--- recipes/msdfgen/conanfile.py
+++ recipes/msdfgen/conanfile.py
@@ -1,5 +1,4 @@
-from conans import ConanFile, Meson, tools
-import os
+from conan import ConanFile
 
 class MsdfGenConanRecipe(ConanFile):
     name = "msdfgen"
@@ -11,59 +10,54 @@ class MsdfGenConanRecipe(ConanFile):
     settings = "os", "compiler", "arch", "build_type"
 
     options = {
-        "fPIC":[True, False],
+        "fPIC":[True, False]
     }
     default_options = {
-        "fPIC": True
+        "fPIC": True,
     }
 
-    source_dir = "{name}-{version}"
+    requires = "freetype/2.12.1@iceshard/stable"
 
     # Additional files to export
     exports_sources = ["patches/*", "Findmsdfgen.cmake"]
 
     # Iceshard conan tools
-    python_requires = "conan-iceshard-tools/0.8.0@iceshard/stable"
+    python_requires = "conan-iceshard-tools/0.9.0@iceshard/stable"
     python_requires_extend = "conan-iceshard-tools.IceTools"
 
-    requires = "freetype/2.12.1@iceshard/stable"
-
-    # Initialize the package
-    def init(self):
-        self.ice_init("cmake")
-        self.build_requires = self._ice.build_requires
-        # self.build_requires = "meson/0.62.1" # self._ice.build_requires
+    ice_generator = "cmake"
+    ice_toolchain = "cmake"
 
     def configure(self):
-        if self.settings.compiler == 'Visual Studio':
+        if self.settings.compiler == 'msvc':
             del self.options.fPIC
 
+    def ice_generate_cmake(self, toolchain, deps):
+        toolchain.variables['MSDFGEN_BUILD_STANDALONE'] = False
+        toolchain.variables['MSDFGEN_INSTALL'] = False
+
     def ice_build(self):
-        self.ice_apply_patches()
-        
-        definitions = { }
-        definitions['MSDFGEN_BUILD_STANDALONE'] = False
-        definitions['MSDFGEN_INSTALL'] = False
-        self.ice_run_cmake(definitions)
+        self.ice_run_cmake()
 
-    def ice_package(self):
-        self.copy("Findmsdfgen.cmake", src=".", dst=".")
-        self.copy("LICENSE.txt", src=self._ice.source_dir, dst="LICENSE")
+    def ice_package_sources(self):
+        self.ice_copy("LICENSE.txt", src=".", dst="LICENSE")
 
-        self.copy("*.h*", "include/msdfgen/core", src="{}/core".format(self._ice.source_dir), keep_path=True)
-        self.copy("*.h*", "include/msdfgen/ext", src="{}/ext".format(self._ice.source_dir), keep_path=True)
-        self.copy("*.h*", "include/msdfgen/skia", src="{}/skia".format(self._ice.source_dir), keep_path=True)
-        self.copy("*.h*", "include/msdfgen/include", src="{}/include".format(self._ice.source_dir), keep_path=True)
-        self.copy("msdfgen.h", "include/msdfgen", src="{}".format(self._ice.source_dir), keep_path=True)
-        self.copy("msdfgen-*.h", "include/msdfgen", src="{}".format(self._ice.source_dir), keep_path=True)
+        self.ice_copy("*.h*", src="core", dst="include/msdfgen/core", keep_path=True)
+        self.ice_copy("*.h*", src = "ext", dst="include/msdfgen/ext", keep_path=True)
+        self.ice_copy("*.h*", src="skia", dst="include/msdfgen/skia", keep_path=True)
+        self.ice_copy("*.h*", src="include", dst="include/msdfgen/include", keep_path=True)
+        self.ice_copy("msdfgen.h", src=".", dst="include/msdfgen", keep_path=True)
+        self.ice_copy("msdfgen-*.h", src=".", dst="include/msdfgen", keep_path=True)
 
-        build_dir = self._ice.build_dir
-        if self.settings.os == "Windows":
-            self.copy("*.lib", dst="lib", src="{}/../build/lib".format(build_dir), keep_path=False)
-            self.copy("*.pdb", dst="lib", src="{}/../build/lib".format(build_dir), keep_path=False)
-        if self.settings.os == "Linux":
-            self.copy("*.a", dst="lib", src="{}/../build/lib".format(build_dir), keep_path=False)
+    def ice_package_artifacts(self):
+        self.ice_copy("*.lib", src=".", dst="lib", keep_path=False)
+        self.ice_copy("*.a", src=".", dst="lib", keep_path=False)
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "msdfgen")
+        self.cpp_info.set_property("cmake_target_name", "msdfgen::msdfgen")
+
+        self.cpp_info.includedirs = ['include', 'include/msdfgen']
+
         self.cpp_info.libdirs = ['lib']
         self.cpp_info.libs = ['msdfgen-core', 'msdfgen-ext']
