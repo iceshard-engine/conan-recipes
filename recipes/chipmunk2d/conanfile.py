e8b983e0461ec070c22f077c3f5af1bee901b188
Dandielo <dandielo@iceshard.net>
2023-04-15 14:19:17 +0900

    Updated chipmunk2d for Conan v2

--- recipes/chipmunk2d/conanfile.py
+++ recipes/chipmunk2d/conanfile.py
@@ -1,6 +1,4 @@
-from conans import ConanFile, MSBuild, tools
-from shutil import copyfile
-import os
+from conan import ConanFile
 
 class Chipmunk2DConan(ConanFile):
     name = "chipmunk2d"
@@ -15,44 +13,38 @@ class Chipmunk2DConan(ConanFile):
     default_options = { "shared":False, "fPIC":True }
 
     # Iceshard conan tools
-    python_requires = "conan-iceshard-tools/0.8.2@iceshard/stable"
+    python_requires = "conan-iceshard-tools/0.9.0@iceshard/stable"
     python_requires_extend = "conan-iceshard-tools.IceTools"
 
-    # Initialize the package
-    def init(self):
-        self.ice_init("cmake")
-        self.build_requires = self._ice.build_requires
+    ice_generator = "cmake"
+    ice_toolchain = "cmake"
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    # Build both the debug and release builds
     def ice_build(self):
-        definitions = { }
-        definitions['BUILD_DEMOS'] = False
-        definitions['BUILD_SHARED'] = self.options.shared
-        definitions['BUILD_STATIC'] = not self.options.shared
+        variables = { }
+        variables['BUILD_DEMOS'] = False
+        variables['BUILD_SHARED'] = self.options.shared
+        variables['BUILD_STATIC'] = not self.options.shared
 
         # We don't want to install anything
-        definitions['INSTALL_DEMOS'] = False
-        definitions['INSTALL_STATIC'] = False
+        variables['INSTALL_DEMOS'] = False
+        variables['INSTALL_STATIC'] = False
         if self.settings.os != "Windows":
-            definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
-        self.ice_run_cmake(definitions)
+            variables['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
+        self.ice_run_cmake(variables=variables)
 
-    def ice_package(self):
-        self.copy("LICENSE.txt", src=self._ice.source_dir, dst="LICENSES")
+    def ice_package_sources(self):
+        self.ice_copy("LICENSE.txt", src=".", dst="LICENSES")
+        self.ice_copy("*.h", src="include", dst="include", keep_path=True)
 
-        self.copy("*.h", "include", src="{}/include".format(self._ice.source_dir), keep_path=True)
-
-        build_dir = self._ice.build_dir
-        if self.settings.os == "Windows":
-            self.copy("*.lib", "lib", build_dir, keep_path=False)
-            self.copy("*.dll", "bin", build_dir, keep_path=False)
-        if self.settings.os == "Linux":
-            self.copy("*.a", "lib", build_dir, keep_path=False)
-            self.copy("*.so*", "bin", build_dir, keep_path=False)
+    def ice_package_artifacts(self):
+        self.ice_copy("*.lib", src=".", dst="lib", keep_path=False)
+        self.ice_copy("*.dll", src=".", dst="bin", keep_path=False)
+        self.ice_copy("*.a", src=".", dst="lib", keep_path=False)
+        self.ice_copy("*.so*", src=".", dst="bin", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libdirs = ["lib"]
