87a47f77dbca05d688653c6b02f40b948a57755f
Dandielo <dandielo@iceshard.net>
2023-05-13 12:36:41 +0900

    Fixing reciple, another try

--- recipes/ice-build-tools-proxy/conanfile.py
+++ recipes/ice-build-tools-proxy/conanfile.py
@@ -2,12 +2,20 @@ from conan import ConanFile
 from conan import tools
 from conan.tools.scm import Git
 from conan.tools.files import chdir
+from conan.tools.layout import basic_layout
 
 class IceBuildToolsProxyConan(ConanFile):
     name = "ice-build-tools-proxy"
     license = "MIT"
     description = "Build tools created for the IceShard project that don't come with tailored features."
     url = "https://github.com/iceshard-engine/ice-build-tools"
+
+    def layout(self):
+        basic_layout(self)
+
+        self.folders.source = "{}-{}".format(self.name, self.version)
+        self.folders.generators = self.folders.source
+        self.folders.build = self.folders.source
 
     def source(self):
         source_info = self.conan_data["sources"][self.version]
@@ -23,5 +31,4 @@ class IceBuildToolsProxyConan(ConanFile):
             git.checkout(source_info["tag"])
 
     def build(self):
-        with chdir("{}-{}".format(self.name, self.version)):
-            self.run("conan create . --version {} --user {} --channel {}".format(self.version, self.user, self.channel))
+        self.run("conan create . --version {} --user {} --channel {}".format(self.version, self.user, self.channel))
