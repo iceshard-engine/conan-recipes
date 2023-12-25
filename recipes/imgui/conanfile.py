8ab4da7ae17e2906a32e59d9e32a3ee413b10160
Dandielo <dandielo@iceshard.net>
2023-12-25 15:28:58 +0900

    ImGui: Moved from premake5 to CMake * Solves one issue with supporting less popular platform targets. * Now compiles properly on Android and WebAssembly (Emscripten)

--- recipes/imgui/conanfile.py
+++ recipes/imgui/conanfile.py
@@ -1,5 +1,6 @@
 from conan import ConanFile
 from conan.tools.files import rm, rmdir, copy
+from os.path import join, normpath
 
 class ImGuiConan(ConanFile):
     name = "imgui"
@@ -13,39 +14,34 @@ class ImGuiConan(ConanFile):
     default_options = { "docking_branch":False, "fPIC":True }
 
     # Additional files to export
-    exports_sources = ["premake5.lua"]
+    exports_sources = ["CMakeLists.txt"]
 
     # Iceshard conan tools
-    python_requires = "conan-iceshard-tools/0.9.0@iceshard/stable", "premake-generator/0.2.0@iceshard/stable"
+    python_requires = "conan-iceshard-tools/0.9.1@iceshard/stable"
     python_requires_extend = "conan-iceshard-tools.IceTools"
 
-    ice_generator = "premake5"
-    ice_toolchain = "native"
+    ice_generator = "cmake"
+    ice_toolchain = "ninja"
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    def generate(self):
-        copy(self, "premake5.lua", src=self.export_sources_folder, dst=self.source_folder)
-        # Switch to the docking branch before continuing
-        if self.options.docking_branch:
-            self.run("git checkout docking")
-        self.ice_generate()
-
     def ice_build(self):
-        if self.settings.compiler == "msvc":
-            self.ice_run_msbuild("imgui.sln")
-        else:
-            self.ice_run_make()
+        copy(self, "CMakeLists.txt", src=normpath(join(self.source_folder, "..")), dst=self.source_folder)
+
+        variables = { }
+        if self.settings.os != "Windows":
+            variables['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
+        self.ice_run_cmake(variables)
 
     def ice_package_sources(self):
         self.ice_copy("LICENSE.txt", src=".", dst="LICENSES")
         self.ice_copy("*.h", src=".", dst="include/imgui", keep_path=True, excludes=("examples/*", "misc/*"))
 
     def ice_package_artifacts(self):
-        self.ice_copy("*.lib", dst="lib", src="bin", keep_path=False)
-        self.ice_copy("*.a", dst="lib", src="bin", keep_path=False)
+        self.ice_copy("*.lib", dst="lib", src=".", keep_path=False)
+        self.ice_copy("*.a", dst="lib", src=".", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libdirs = [ "lib" ]
