b24eefb362094495625e19918f12e1fa0ae7108d
Daniel <dandielo@iceshard.net>
2025-05-13 22:01:55 +0900

    Fixed lua-filesystem builds for linux targets.

--- recipes/lua-filesystem/conanfile.py
+++ recipes/lua-filesystem/conanfile.py
@@ -1,6 +1,7 @@
 from conan import ConanFile
 from conan.tools.microsoft import NMakeDeps, NMakeToolchain
-from conan.tools.files import chdir, replace_in_file
+from conan.tools.gnu import MakeDeps #, MakeToolchain
+from conan.tools.files import copy, chdir, replace_in_file
 
 import os
 
@@ -29,18 +30,35 @@ class LuaFilesystemConan(ConanFile):
         self.folders.build = self.folders.source
 
     def generate(self):
-        deps = NMakeDeps(self)
-        deps_vars = deps.vars()
+        if self.settings.os == "Windows":
+            deps = NMakeDeps(self)
+            deps_vars = deps.vars()
 
-        tc = NMakeToolchain(self)
-        env = tc.environment()
-        env.append('CFLAGS', deps_vars.get('CL'))
-        env.define('LUA_LIB', deps_vars.get('_LINK_'))
-        env.define('LIB', deps_vars.get('LIB'))
-        tc.generate(env)
+            tc = NMakeToolchain(self)
+            env = tc.environment()
+            env.append('CFLAGS', deps_vars.get('CL'))
+            env.define('LUA_LIB', deps_vars.get('_LINK_'))
+            env.define('LIB', deps_vars.get('LIB'))
+            tc.generate(env)
 
-        chdir(self, self.source_folder)
-        replace_in_file(self, os.path.join(self.source_folder, "Makefile.win"), "include config.win", "#test")
+            chdir(self, self.source_folder)
+            replace_in_file(self, os.path.join(self.source_folder, "Makefile.win"), "include config.win", "#test")
+        else:
+            deps = MakeDeps(self)
+            deps.generate()
+
+            copy(self, "conandeps.mk", src=".", dst=self.source_folder)
+
+            # Update config file to use conan provided paths and values
+            path_config = os.path.join(self.source_folder, "config")
+            replace_in_file(self, path_config, "PREFIX=", "include {}\nPREFIX=".format('conandeps.mk'))
+            replace_in_file(self, path_config, "$(LUA_INC)", "-I$(CONAN_INCLUDE_DIRS)")
+            replace_in_file(self, path_config, "gcc", str(self.settings.compiler))
+
+            # Update Makefile to use conan provided paths and values
+            path_makefile = os.path.join(self.source_folder, "Makefile")
+            replace_in_file(self, path_makefile, "$(LUA_LIBDIR)", "-L$(CONAN_LIB_DIRS)")
+
 
     def ice_build(self):
         if self.settings.os == "Windows":
