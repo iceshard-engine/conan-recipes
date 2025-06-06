ed853fcddaf27335178e0bed458a3bbbb204b84f
Dandielo <dandielo@iceshard.net>
2025-06-06 23:28:43 +0900

    Added 'rapidjson' recipe for use by 'iceshard'

---
+++ recipes/rapidjson/conanfile.py
@@ -0,0 +1,33 @@
+from conan import ConanFile
+
+class RapidJSONConanRecipe(ConanFile):
+    name = "rapidjson"
+    description = "A fast JSON parser/generator for C++ with both SAX/DOM style API"
+    topics = ("rapidjson", "json", "parser", "generator")
+    url = "https://github.com/Tencent/rapidjson"
+    homepage = "http://rapidjson.org"
+    license = "MIT"
+
+    # Default user and channel
+    user = "iceshard"
+    channel = "stable"
+
+    # Settings and options
+    settings = "os", "compiler", "arch", "build_type"
+
+    # Iceshard conan tools
+    python_requires = "conan-iceshard-tools/1.0.1@iceshard/stable"
+    python_requires_extend = "conan-iceshard-tools.IceTools"
+
+    ice_generator = "none"
+    ice_toolchain = "none"
+
+    def package_id(self):
+        self.info.clear()
+
+    def ice_package_sources(self):
+        self.ice_copy("license.txt", src=".", dst="LICENSES")
+        self.ice_copy("*.hpp", src=".", dst="include/rapidxml_ns", excludes=("tests/*", "doc/*"))
+
+    def package_info(self):
+        self.cpp_info.includedirs = [ "include" ]
